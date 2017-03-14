912d6126b28e6dc0d12787aef2828c7010e2530d
Evgeniy A. Cymbalyuk <cimbaluk@gmail.com>
2017-03-14 20:54:45 +0000

    use vim as default editor for git comments

--- .bashrc
+++ .bashrc
@@ -1,3 +1,7 @@
 
+# add git helper aliases
 alias gs="git status"
 
+# always use vim as an edir
+export VISUAL=vim
+export EDITOR="$VISUAL"
---
+++ setup-git.sh
@@ -0,0 +1,9 @@
+# setup creds
+git config --global user.name "Evgeniy A. Cymbalyuk"
+git config --global user.email "cimbaluk@gmail.com"
+
+# set commit editor to vim
+git config --global core.editor "vim"
+
+# it can be `matching` if we want to push all branches with matching names
+git config --global push.default simple
