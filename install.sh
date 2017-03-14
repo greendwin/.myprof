c6abdeb5528da2d5255699ca7aabaead4273b88f
Evgeniy A. Cymbalyuk <cimbaluk@gmail.com>
2017-03-14 12:14:23 +0000

    add tmux config & bash alias for git status

--- install.sh
+++ install.sh
@@ -4,3 +4,10 @@ git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
 ~/.bash_it/install.sh
 sed -i "s/BASH_IT_THEME='.*'/BASH_IT_THEME='bakke'/g" ~/.bashrc
 
+# append our custom bashrc to bash_it
+echo "source ~/.myprof/.bashrc" >> ~/.bashrc
+
+echo Installing tmux config...
+ln -s ~/.myprof/.tmux.conf ~/.tmux.conf
+
+echo Done.
