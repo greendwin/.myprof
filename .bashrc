c178b793f4942dcb318fc1cbc6cb3ce6a5731d4c
Evgeniy A. Cymbalyuk <cimbaluk@gmail.com>
2017-03-17 13:34:42 +0000

    * add git commands gadd & gcm for faster commits * add git docker cleanup method and helpers to attach to existing container * run bash-it installer in silent mode to allow to run it in vagrant provision

--- .bashrc
+++ .bashrc
@@ -1,6 +1,23 @@
 
 # add git helper aliases
 alias gs="git status"
+alias gadd="git add . && git status"
+alias gcm="git commit -m"
+
+alias dps="docker ps"
+alias dsh="docker exec $(docker ps -lq) /bin/sh"
+
+# TODO: create docker-build and docker-run with cached label in ~/.docker/default-label
+
+docker-cleenup() {
+	echo Remove stopped containers...
+	docker ps -a -q | xargs -r docker rm
+
+	echo Remove untagged images...
+	docker images -q --filter "dangling=true" | xargs -r docker rmi
+
+	echo Done.
+}
 
 # always use vim as an edir
 export VISUAL=vim
--- install.sh
+++ install.sh
@@ -1,15 +1,17 @@
 
 echo Installing bash_it...
 git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
-~/.bash_it/install.sh
+~/.bash_it/install.sh -s		# use silent mode to allow installation in vagrant provision
 sed -i "s/BASH_IT_THEME='.*'/BASH_IT_THEME='bakke'/g" ~/.bashrc
 
 # append our custom bashrc to bash_it
 echo "source ~/.myprof/.bashrc" >> ~/.bashrc
 
 echo Installing tmux config...
+# TODO: use new file & source execution for custom overrides support
 ln -sf ~/.myprof/.tmux.conf ~/.tmux.conf
 
+# TODO: rename .tmux.confg and .vimrc to tmux and vimrc so they are no longer hidden in the repo
 echo Installing vim config...
 ln -sf ~/.myprof/.vimrc ~/.vimrc
 
