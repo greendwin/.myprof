1225c9f5768fc3990a73e17fecc8ccd906de8b95
Evgeniy A. Cymbalyuk <cimbaluk@gmail.com>
2017-03-24 13:10:38 +0000

    install zsh install common-aliases plugin

--- install-zsh.sh
+++ install-zsh.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
+sudo apt-get install -y zsh
+
 # install Oh-My-Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 sed -i 's/ZSH_THEME=".*"/ZSH_THEME="avit"/g' ~/.zshrc
+sed -i 's/plugins=\(.*\)/plugins=\(common-aliases git\)/g' ~/.zshrc
 
 # append our custom bashrc to bash_it
 echo "source ~/.myprof/zshrc" >> ~/.zshrc
