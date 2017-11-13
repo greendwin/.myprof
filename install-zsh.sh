dc321dc1ee2c8284d9f8122f8ad9011bfd4c2918
root <root@vm-linux-server>
2017-11-13 12:18:12 +0000

    fix oh-my-zsh plugins setup

--- install-zsh.sh
+++ install-zsh.sh
@@ -5,8 +5,8 @@ sudo apt-get install -y zsh
 # install Oh-My-Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-sed -i 's/ZSH_THEME=".*"/ZSH_THEME="avit"/g' ~/.zshrc
-# sed -i 's/plugins=\(.*\)/plugins=\(common-aliases git\)/g' ~/.zshrc
+sed -i 's|ZSH_THEME=".*"|ZSH_THEME="avit"|g' ~/.zshrc
+sed -i 's|^plugins=(|plugins=(\n  common-aliases |g' ~/.zshrc
 
 # append our custom bashrc to bash_it
 echo "source ~/.myprof/zshrc" >> ~/.zshrc
