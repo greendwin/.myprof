6342b2e48df14c3752c7aa6e2c7da53a409a0756
Tsyimbalyuk Evgeniy <cimbaluk@saber.games>
2026-03-05 17:56:25 +0300

    add `zsh` package

---
+++ zsh/install.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+set -e
+
+# TBD: do we need `atool` install for easier unzips?
+
+sudo apt-get install zsh -y
+
+# install Oh-My-Zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# sed -i 's|ZSH_THEME=".*"|ZSH_THEME="avit"|g' ~/.zshrc
+sed -i 's|^plugins=(|plugins=(common-aliases |g' ~/.zshrc
+sed -i 's|^# export PATH=$HOME|export PATH=$HOME|g' ~/.zshrc
+
+# support `.zshuser` that can be linked by stow
+echo '[[ -f "$HOME/.zshuser" ]] && source "$HOME/.zshuser"' >> ~/.zshrc
+
+cd ~/.myprof
+stow zsh
+
+echo "set ZSH_THEME var in `.zshrc` for your theme"
+echo "list of good themes:"
+echo " - crcandy"
+echo " - avit"
+echo " - ys"
+
+echo "done."
